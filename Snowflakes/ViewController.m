f22edebdfd4a44a21981a2e4557c0fcf3fc7074a
Tom Hamming <tomhamming@olivetree.com>
2014-12-02 16:12:20 -0800

    Project fixes and minor enhancements

--- Snowflakes/ViewController.m
+++ Snowflakes/ViewController.m
@@ -29,9 +29,9 @@
 {
     [super viewDidAppear:animated];
     
-    NSTimeInterval sleepTime = 0.3;
+    NSTimeInterval sleepTime = 0.2;
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
-        sleepTime = 0.15;
+        sleepTime = 0.1;
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
                    {
@@ -40,14 +40,15 @@
                            [NSThread sleepForTimeInterval:sleepTime];
                            dispatch_async(dispatch_get_main_queue(), ^
                                           {
-                                              CGFloat width = (arc4random() % 80) + 20;
+                                              CGFloat width = (arc4random() % 40) + 20;
                                               CGFloat xCoord = arc4random() % (int)self.view.frame.size.width;
+                                              xCoord -= (width / 2);
                                               CGRect startRect = CGRectMake(xCoord, width * -1, width, width);
                                               UIImageView *view = [[UIImageView alloc]initWithFrame:startRect];
                                               [self.view addSubview:view];
                                               view.image = [UIImage imageNamed:@"flake.png"];
                                               CGRect endRect = CGRectMake(xCoord, self.view.frame.size.height, width, width);
-                                              NSTimeInterval time = 16 + (arc4random() % 8);
+                                              NSTimeInterval time = 16 + (arc4random() % 9);
                                               [UIView animateWithDuration:time delay:0 options:UIViewAnimationOptionCurveLinear animations:^
                                                {
                                                    view.frame = endRect;
